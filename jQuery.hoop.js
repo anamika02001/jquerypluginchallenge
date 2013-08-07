6bc46b9407f285f19dbee3f63d3ab66f6d151e7f
Anamika Mukherji <anamika02001@gmail.com>
2013-08-06 23:14:11 -0700

    Updated slide logic, horizontally aligned children of li, scaled larger images

--- jQuery.hoop.js
+++ jQuery.hoop.js
@@ -10,17 +10,16 @@
 		this.$ul = $(this.$div.selector + " ul");
 		this.$li = $(this.$div.selector + " ul li");
 		this.$options = options;
-		this.$maxSize = this.$ul.children().size();
-		if(this.$options.rotation === "right"){
-			this.$progressBarIterator = 2;
-		}else{
-			this.$progressBarIterator = 0;
-		}
-		this.init();
-		this.initEventHandlers();
 		
-		this.$timer = this.playOnHover();
-	//this.$timer = setInterval($.proxy(function(){this.slide(this.$options.rotation, this.$options.slide)}, this), this.$options.interval);
+		this.$maxSize = this.$ul.children().size();
+		this.$width = 600;
+		this.$height = 300;
+		this.$index = 1;
+		
+		this.init();
+		this.initCss();
+		this.initEventHandlers();
+		this.playOnHover();
 	};
 	
 	Hoop.defaults = {
@@ -30,118 +29,108 @@
 	
 	Hoop.prototype = {
 		init: function(){
-			
-			//this.$div.progressbar( "option", { disabled: true } );
 			this.$div.append("<input type='image' class = 'previous' src='images/leftArrow.png' alt='Previous'/>");
 			this.$div.append("<input type='image' class = 'next' src='images/rightArrow.png' alt='Next'/>");	
 			this.$div.append("<input type='image' class = 'pause' src='images/pause.png' alt='Pause'/>");
 			this.$div.append("<input type='image' class = 'play' src='images/play.png' alt='Play'/>");
 			//this.$div.append("<input type='image' class = 'fullscreen' src='images/fullscreen.png' alt='Fullscreen'/>");
 			this.$div.append("<div class='progressBar'></div>");
-
+			
 			
 			$(this.$div.selector + " input.play").hide();
-			this.initCss();
-			$(this.$div.selector + " div.progressBar").progressbar();
-			$(this.$div.selector + " div.progressBar").progressbar( "option", "max", this.$maxSize);
-			//$(this.$div.selector + "div.progressBar").progressbar({"value":"70"});
-			if(this.$options.rotation === "left"){
-				$(this.$li.selector + ":first").before($(this.$li.selector + ":last"));
-			} else{
-				$(this.$li.selector + ":last").after($(this.$li.selector + ":first"));
-			}
-			this.slide(this.$options.rotation, this.$options.slide);
+			$(this.$div.selector + " div.progressBar").progressbar({
+				max: this.$maxSize,
+				value: this.$index
+			});
 		},
 		
 		initCss: function(){
-			var width, height;
 			if($(window).width() >= 600){
-				width = "600";				
-				height = "300";
+				this.$width = "600";				
+				this.$height = "300";
 				buttonSize = "30";
-				
 			} 
 
 			if($(window).width() < 600){
-				width = 0.949 * $(window).width();
-				height = 0.594 * width;
-				buttonSize = 0.05 * width;
+				this.$width = 0.949 * $(window).width();
+				this.$height = 0.5 * this.$width;
+				buttonSize = 0.05 * this.$width;
 			}
 
 			this.$div.css({
 				"position":"relative",
-				"float": "left",
-				"width": width,
-				"height": height,
+				"width": this.$width + "px",
+				"height": this.$height + "px",
 				"overflow": "hidden",
 				"border" : "2px solid",
-
-				
 			});
 			this.$ul.css({
-				"left": "0px",
-				"position": "relative",
+				"position" : "absolute",
 				"list-style-type": "none",
 				"width": "9999px",
+				"float" : "left",
 				"margin": "0px",
 				"padding": "0px"
 			});
+			
 			this.$li.css({
-				"padding": "0px",
+				"position":"relative",
+				"float":"left",
+				"width": this.$width,
+				"height": this.$height,
+				"text-align" : "center",
 				"margin": "0px",
-				"float": "left",
-				"background": "transparent",
-				"width": width + "px",
-				"height": height + "px",
+				"padding": "0px"
 			});
 
-
-			
 			$(this.$div.selector + " input.previous").css({
 				"position": "absolute",
-				//"text-align": "center",
 				"left": "0px",
 				"float": "left",
-				"top": (height/2) + "px",
+				"top": (this.$height/2) + "px",
 				"width": buttonSize + "px",
 				"height": buttonSize + "px",
 				"z-index": "2",
 			});	
+			
 			$(this.$div.selector + " input.next").css({
 				"position": "absolute",
-				//"text-align": "center",
 				"float": "left",
-				"top": height/2 + "px",
-				"left": (width - buttonSize) + "px",
+				"top": this.$height/2 + "px",
+				"left": (this.$width - buttonSize) + "px",
 				"width": buttonSize + "px",
 				"height": buttonSize + "px",
 				"z-index": "2",
-			});		
+			});	
+				
 			$(this.$div.selector + " input.pause").css({
 				"position": "absolute",
 				"float": "left",
-				"top": height*4/5 + "px",
-				"left": (width/2) + "px",
+				"top": this.$height*4/5 + "px",
+				"left": (this.$width/2) + "px",
 				"width": buttonSize + "px",
 				"height": buttonSize + "px",
 				"z-index": "2",
 			});	
+			
 			$(this.$div.selector + " input.play").css({
 				"position": "absolute",
 				"float": "left",
-				"top": height*4/5 + "px",
-				"left": (width/2) + "px",
+				"top": this.$height*4/5 + "px",
+				"left": (this.$width/2) + "px",
 				"width": buttonSize + "px",
 				"height": buttonSize + "px",
 				"z-index": "2",
 			});	
+			
 			$(this.$div.selector + " div.progressBar").css({
 				"position": "absolute",
-				"top": (height*0.89 ) + "px",
+				"top": (this.$height*0.89 ) + "px",
 				"z-index": "3",
 				"height": "5px",
-				"width" : width + "px",
+				"width" : this.$width + "px",
 			});
+			
 		/*	$(this.$div.selector + " input.fullscreen").css({
 				"position": "absolute",
 				"top": (height - buttonSize) + "px",
@@ -155,55 +144,46 @@
 		
 		initEventHandlers: function(){
 			var scope = this;
-			this.$timer = this.$div.hover($.proxy(this.pauseOnHover,this), $.proxy(this.playOnHover,this));
-			
+			this.$div.hover($.proxy(this.pauseOnHover,this), $.proxy(this.playOnHover,this));
 			$(window).resize($.proxy(this.initCss, scope));
-			
 			$(this.$div.selector + " input.next").click($.proxy(this.next, this));
 			$(this.$div.selector + " input.previous").click($.proxy(this.previous, this));
 			$(this.$div.selector + " input.fullscreen").click($.proxy(this.fullscreen, this));
-			
-			this.$timer = $(this.$div.selector + " input.pause").click($.proxy(this.pauseOnButton,this));
-			this.$timer = $(this.$div.selector + " input.play").click($.proxy(this.playOnButton,this));
-			
+			$(this.$div.selector + " input.pause").click($.proxy(this.pauseOnButton,this));
+			$(this.$div.selector + " input.play").click($.proxy(this.playOnButton,this));
 		},
 		
 		slide: function(direction, callback){
 			
-			
-			var width = this.$li.outerWidth();
-			if(direction === "right"){
-				var left = parseInt(this.$ul.css("left")) + width;
-			} else {
-				var left = parseInt(this.$ul.css("left")) - width;
+			if(direction === "left"){
+				this.$index++;
+				if(this.$index === this.$maxSize + 1){
+					this.$index = 1;
+				}
+				$(this.$div.selector + " div.progressBar").progressbar({value: this.$index});
+				this.$ul.animate({"left": -((this.$index -1)* this.$width) + "px"},this.$interval > 1000 ? this.$interval - 1000 : this.$interval);
+				
+				
+			}else{
+				this.$index--;
+				if(this.$index === 0){
+					this.$index = this.$maxSize;
+				}
+				$(this.$div.selector + " div.progressBar").progressbar({value: this.$index});
+				this.$ul.animate({"left": (-(this.$index -1) * this.$width) + "px"},this.$interval > 1000 ? this.$interval - 1000 : this.$interval);
+			}
+			var height = this.$li.children().eq(this.$index-1).height();
+			var width = this.$li.children().eq(this.$index-1).width();
+			//this.$li.children().eq(this.$index-1).css({});
+			if(height > 300 || width > 600 ){
+				if(width > height){
+					this.$li.children().eq(this.$index-1).css({width: this.$width + "px", height: height*this.$width/width+ "px"});
+				}else {
+					this.$li.children().eq(this.$index-1).css({width: this.$height*width/height + "px", height: this.$height + "px"});
+				}
+				
 			}
 			
-			$(this.$ul.selector + ":not(:animated)").animate({"left": left}, 
-			this.$interval > 1000 ? this.$interval - 1000 : this.$interval, 
-			$.proxy(function(){
-				
-				if(direction === "right"){
-									
-					
-					this.$progressBarIterator -= 1;
-					if(this.$progressBarIterator < 1){
-						this.$progressBarIterator = this.$maxSize;
-					}
-					$(this.$li.selector + ":first").before($(this.$li.selector + ":last"));
-					
-				} else {
-					//alert(this.$li.index());
-					$(this.$li.selector + ":last").after($(this.$li.selector + ":first"));
-					
-					
-					this.$progressBarIterator += 1;
-					if(this.$progressBarIterator > this.$maxSize){
-						this.$progressBarIterator = 1;
-					}
-				}
-				this.$ul.css({"left": "0px", "margin-left":"auto", "margin-right":"auto"});
-				$(this.$div.selector + " div.progressBar").progressbar({value: this.$progressBarIterator});
-			}, this));
 			if(callback != undefined){
 				callback();
 			}
@@ -213,50 +193,45 @@
 			this.$hovered = true;
 			this.$timer = clearInterval(this.$timer);
 			if(this.$paused === true){
-				$(this.$div.selector + " input.pause").css({"display": "none"});
-				$(this.$div.selector + " input.play").css({"display": "block"});
+				$(this.$div.selector + " input.pause").hide();
+				$(this.$div.selector + " input.play").show();
 			}else{
-				$(this.$div.selector + " input.pause").css({"display": "block"});
-				$(this.$div.selector + " input.play").css({"display": "none"});
+				$(this.$div.selector + " input.pause").show();
+				$(this.$div.selector + " input.play").hide();
 			}
-			$(this.$div.selector + " input.next").css({"display": "block"});
-			$(this.$div.selector + " input.previous").css({"display": "block"});
-			$(this.$div.selector + " div.progressBar").css({"display": "block"});
-			return this.$timer;
+			$(this.$div.selector + " input.next").show();
+			$(this.$div.selector + " input.previous").show();
+			$(this.$div.selector + " div.progressBar").show();
+			
 		},
 		
 		playOnHover: function(){
-			$(this.$div.selector + " input.next").css({"display": "none"});
-			$(this.$div.selector + " input.previous").css({"display": "none"});
-			$(this.$div.selector + " input.pause").css({"display": "none"});
-			$(this.$div.selector + " input.play").css({"display": "none"});
-			$(this.$div.selector + " div.progressBar").css({"display": "block"});
-			if(this.$paused !== true)
-			{
+			$(this.$div.selector + " input.next").hide();
+			$(this.$div.selector + " input.previous").hide();
+			$(this.$div.selector + " input.pause").hide();
+			$(this.$div.selector + " input.play").hide();
+			$(this.$div.selector + " div.progressBar").show();
+			if(this.$paused !== true) {
 				this.$timer = setInterval($.proxy(function(){this.slide(this.$options.rotation, this.$options.slide)}, this), this.$options.interval);
-				return this.$timer;
 				this.$hovered = false;
 			}
 			return undefined;
 		},
 		
 		pauseOnButton: function(){
-				this.$paused = true;
-				$(this.$div.selector + " input.pause").css({"display": "none"});
-				$(this.$div.selector + " input.play").css({"display": "block"});
-				this.$timer = clearInterval(this.$timer);
-				return this.$timer;
+			this.$paused = true;
+			$(this.$div.selector + " input.pause").hide();
+			$(this.$div.selector + " input.play").show();
+			this.$timer = clearInterval(this.$timer);
 		},
 		
 		playOnButton: function(){
-		
-				this.$paused = false;
-				$(this.$div.selector + " input.play").css({"display": "none"});
-				$(this.$div.selector + " input.pause").css({"display": "block"});
-				if(this.$hovered !== true){
-					this.$timer = setInterval($.proxy(function(){this.slide(this.$options.rotation, this.$options.slide)}, this), this.$options.interval);
-				}
-				return this.$timer;
+			this.$paused = false;
+			$(this.$div.selector + " input.play").hide();
+			$(this.$div.selector + " input.pause").show();
+			if(this.$hovered !== true){
+				this.$timer = setInterval($.proxy(function(){this.slide(this.$options.rotation, this.$options.slide)}, this), this.$options.interval);
+			}
 		},
 		
 		next: function(){
@@ -264,7 +239,6 @@
 		},
 		
 		previous: function(){
-			
 			this.slide("right");
 		},
 		
